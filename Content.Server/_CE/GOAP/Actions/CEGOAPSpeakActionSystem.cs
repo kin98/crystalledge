9aba9b8f112ee00544a3fd404c038b9f868bc401
kin98 <benj.schleiss@bluewin.ch>
2026-03-25 13:42:41 +0100

    testing with flem

--- Content.Server/_CE/GOAP/Actions/CEGOAPSpeakActionSystem.cs
+++ Content.Server/_CE/GOAP/Actions/CEGOAPSpeakActionSystem.cs
@@ -1,7 +1,13 @@
 using System;
+using Content.Server.Chat.Managers;
+using Content.Server.Chat.Systems;
 using Content.Shared._CE.GOAP;
+using Content.Shared._CE.Health;
+using Content.Shared._CE.Health.Components;
 using Content.Shared.Actions.Components;
+using Content.Shared.Speech;
 using Robust.Shared.Prototypes;
+using Robust.Shared.Toolshed.Commands.Values;
 
 namespace Content.Server._CE.GOAP.Actions;
 
@@ -18,6 +24,8 @@ public sealed partial class CEGOAPSpeakActionSystem : CEGOAPActionSystem<CEGOAPS
 {
     private EntityQuery<EntityTargetActionComponent> _entityTargetQuery;
     private EntityQuery<WorldTargetActionComponent> _worldTargetQuery;
+    [Dependency] private readonly CESharedDamageableSystem _damage = default!;
+    [Dependency] private readonly ChatSystem _chat = default!;
 
     public const string SpeakTargetKey = "speak";
 
@@ -41,11 +49,17 @@ public sealed partial class CEGOAPSpeakActionSystem : CEGOAPActionSystem<CEGOAPS
         var target = GetTarget(ent, args.Action.TargetKey);
         if (target == null)
             return;
+        EnsureComp<SpeechComponent>(ent, out var comp);
+        comp.SpeechVerb = "Cluwne";
     }
 
     protected override void OnActionUpdate(Entity<CEGOAPComponent> ent, ref CEGOAPActionUpdateEvent<CEGOAPSpeakAction> args)
     {
         base.OnActionUpdate(ent, ref args);
+        _chat.TrySendInGameICMessage(ent, "test", Shared.Chat.InGameICChatType.Speak, false, nameOverride: "Test");
+        if (TryComp<CEDamageableComponent>(ent, out var comp))
+            _damage.ChangeDamage((ent, comp), 1, out var damage);
+
     }
     protected override void OnActionShutdown(Entity<CEGOAPComponent> ent, ref CEGOAPActionShutdownEvent<CEGOAPSpeakAction> args)
     {
